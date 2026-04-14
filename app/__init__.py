2bc8ac4dd465c3532831342abc9c26786ea41dd9
Kaito Ono <kaito@example.com>
2026-04-14 17:47:45 +0900

    CRUD操作

--- app/__init__.py
+++ app/__init__.py
@@ -2,11 +2,13 @@ from flask import Flask, render_template
 from config import Config
 from app.db import init_db, close_db
 from app.routes.auth import auth_bp
+from app.routes.tasks import tasks_bp
 
 def create_app():
     app = Flask(__name__, template_folder="../templates", static_folder="../static")
     app.config.from_object(Config)
     app.register_blueprint(auth_bp)
+    app.register_blueprint(tasks_bp)
 
     @app.route("/")
     def home():
---
+++ app/routes/tasks.py
@@ -0,0 +1,166 @@
+from flask import Blueprint, render_template, request, session, redirect, url_for, flash
+from datetime import datetime
+from app.db import get_db
+
+tasks_bp = Blueprint("tasks", __name__)
+
+#ログイン状態であればタスク一覧画面へ遷移、ログイン状態でなければログイン画面へ遷移
+@tasks_bp.route("/")
+def home():
+    if "user_id" in session:
+        return redirect(url_for("tasks.task_list"))
+    return redirect(url_for("auth.login"))
+
+#ログイン状態であれば、DBからタスク一覧を画面に表示させる
+@tasks_bp.route("/tasks")
+def task_list():
+    if "user_id" not in session:
+        flash("ログインが必要です。")
+        return redirect(url_for("auth.login"))
+
+    db = get_db()
+    tasks = db.execute(
+        """
+        SELECT id, title, description, due_date, status, created_at, updated_at
+        FROM tasks
+        WHERE user_id = ?
+        ORDER BY id DESC
+        """,
+        (session["user_id"],)
+    ).fetchall()
+
+    return render_template("tasks/list.html", tasks=tasks)
+
+#ログイン状態であれば、タスク作成画面へ遷移してDBにタスクを追加する
+@tasks_bp.route("/tasks/create", methods=["GET", "POST"])
+def task_create():
+    if "user_id" not in session:
+        flash("ログインが必要です。")
+        return redirect(url_for("auth.login"))
+
+    if request.method == "POST":
+        title = request.form.get("title").strip()
+        description = request.form.get("description").strip()
+
+        if not title:
+            flash("タイトルは必須です。")
+            return render_template("tasks/create.html")
+
+        now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+        db = get_db()
+        db.execute(
+            """
+            INSERT INTO tasks (user_id, title, description, created_at, updated_at)
+            VALUES (?, ?, ?, ?, ?)
+            """,
+            (session["user_id"], title, description, now, now)
+        )
+        db.commit()
+
+        flash("タスクを作成しました。")
+        return redirect(url_for("tasks.task_list"))
+
+    return render_template("tasks/create.html")
+
+#タスク一覧からタスクの詳細画面へ遷移する
+@tasks_bp.route("/tasks/<int:task_id>")
+def task_detail(task_id):
+    if "user_id" not in session:
+        flash("ログインしてください")
+        return redirect(url_for("auth.login"))
+
+    db = get_db()
+    task = db.execute(
+        """
+        SELECT id, user_id, title, description, due_date, status, created_at, updated_at
+        FROM tasks
+        WHERE id = ? AND user_id = ?
+        """,
+        (task_id, session["user_id"])
+    ).fetchone()
+
+    if task is None:
+        flash("タスクが見つかりません")
+        return redirect(url_for("tasks.task_list"))
+
+    return render_template("tasks/detail.html", task=task)
+
+#タスクがあれば、タスク更新画面へ遷移し更新する
+@tasks_bp.route("/tasks/<int:task_id>/edit", methods=["GET", "POST"])
+def task_edit(task_id):
+    if "user_id" not in session:
+        flash("ログインしてください")
+        return redirect(url_for("auth.login"))
+
+    db = get_db()
+    task = db.execute(
+        """
+        SELECT id, user_id, title, description, due_date, status, created_at, updated_at
+        FROM tasks
+        WHERE id = ? AND user_id = ?
+        """,
+        (task_id, session["user_id"])
+    ).fetchone()
+
+    if task is None:
+        flash("タスクが見つかりません")
+        return redirect(url_for("tasks.task_list"))
+
+    if request.method == "POST":
+        title = request.form.get("title", "").strip()
+        description = request.form.get("description", "").strip()
+
+        if not title:
+            flash("タイトルは必須です。")
+            return render_template("task/edit.html", task=task)
+
+        now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+        db.execute(
+            """
+            UPDATE tasks
+            SET title = ?, description = ?, updated_at = ?
+            WHERE id = ? AND user_id = ?
+            """,
+            (title, description, now, task_id, session["user_id"])
+        )
+        db.commit()
+
+        flash("タスクを更新しました。")
+        return redirect(url_for("tasks.task_detail", task_id=task_id))
+
+    return render_template("tasks/edit.html", task=task)
+
+@tasks_bp.route("/tasks/<int:task_id>/delete", methods=["POST"])
+def task_delete(task_id):
+    if "user_id" not in session:
+        flash("ログインしてください")
+        return redirect(url_for("auth.login"))
+
+    db = get_db()
+
+    task = db.execute(
+        """
+        SELECT id
+        FROM tasks
+        WHERE id = ? AND user_id = ?
+        """,
+        (task_id, session["user_id"])
+    ).fetchone()
+
+    if task is None:
+        flash("タスクがありません")
+        return redirect(url_for("tasks.task_list"))
+
+    db.execute(
+        """
+        DELETE FROM tasks
+        WHERE id = ? AND user_id = ?
+        """,
+        (task_id, session["user_id"])
+    )
+    db.commit()
+
+    flash("タスクを削除しました。")
+    return redirect(url_for("tasks.task_list"))
